9b51493ad3464d27680c58df08d83efa49206d19
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to bind a chart to XPO
+
+
+<p>This example demonstrates how a chartControl can be bound to <strong>XPO</strong> data (<a href="http://devexpress.com/Products/NET/XPO/">eXpress Persistent Objects</a>), using the <strong>XPCollection</strong> component.</p>
+
+<br/>
+
+
---
+++ VB/BindChartToXPO/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.Xpo
+
+Namespace BindChartToXPO
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+            For i As Integer = 1 To 10
+                Dim record As New SeriesRecord()
+                record.Argument = "Item " & i.ToString()
+                record.Value = i
+                xpCollection1.Add(record)
+            Next i
+
+        End Sub
+    End Class
+
+    Public Class SeriesRecord
+        Inherits XPObject
+
+        Public Argument As String
+        Public Value As Integer
+    End Class
+
+End Namespace
